e618d313c7c51189440ce2b2fb6bb6e3cdd1eda3
HugoAl1986 <hugo.arcal@gmail.com>
2021-04-14 09:36:50 +0200

    Premiers commit leaflet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,64 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import PositionVille from './component/PositionVille';
 
-const Wrapper = styled.div`
-  margin: 40px auto;
-  width: 1000px;
-`
+const ContainerGlobal = styled.div`
+padding : 1rem;
+display : flex;
+border : solid 1px black
+`;
 
-const App = () => (
-  <Wrapper>
-    Hi !
-  </Wrapper>
-)
+const ContainerVille = styled.div`
+display : flex;
+flex-direction : column;
+padding : 1rem;
+margin-right:0.5rem;
+border : solid 1px black;
+width : 30%;
+`;
+const Button = styled.button`
+margin : 1rem 0;
+padding : 1rem 0.5rem;
+border : solid 1px grey;
+background : transparent;
+width : auto;
+`;
+
+
+const App = () => {
+  
+  const [positionInitiale,setPositionInitiale] = useState([49.195184999999995, 2.043914]);
+  const [map, setMap] = useState(null);
+  
+  const changeCity = (e) => {
+    const LatLng = PositionVille(e.currentTarget.name);
+    console.log(LatLng);
+    setPositionInitiale(LatLng);
+    if (map) map.flyTo(LatLng,8);
+  }
+
+  return(
+      <ContainerGlobal> 
+          <ContainerVille>
+            <Button onClick={changeCity} name="Los Angeles">Los Angeles</Button>
+            <Button onClick={changeCity} name="Detroit">Detroit</Button>
+            <Button onClick={changeCity} name="Las Vegas"><a href="">Las Vegas</a></Button>
+          </ContainerVille>  
+          <MapContainer id="mapid" center={positionInitiale} zoom={8} scrollWheelZoom={false} whenCreated={map => setMap(map)}>
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={positionInitiale}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+          </MapContainer>
+      </ContainerGlobal>
+  ) 
+}
+
 
 export default App;
---
+++ src/component/PositionVille.jsx
@@ -0,0 +1,14 @@
+import * as data from '../cities.json';
+const PositionVille = (name) => {
+
+        const DatasJson = data.default;
+        const ville = element => element.city === name;
+        const indexVille = DatasJson.findIndex(ville);
+        const DatasVille = data.default[indexVille];
+        const positionVille = [DatasVille.latitude,DatasVille.longitude]
+            
+  
+    return positionVille;
+}
+ 
+export default PositionVille;
