74f4b2b0a54c18df1f039d3f0cbe256b3ce734a2
HugoAl1986 <hugo.arcal@gmail.com>
2021-04-16 15:53:27 +0200

    Installation prettier

--- src/App.js
+++ src/App.js
@@ -1,67 +1,70 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
-import CityPosition from './component/CityPosition';
-import * as data from './cities.json';
-import Pagination from './component/Pagination';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import CityPosition from "./component/CityPosition";
+import * as data from "./cities.json";
+import Pagination from "./component/Pagination";
 
 const GeneralContainer = styled.div`
-padding : 1rem;
-display : flex;
-border : solid 1px black
+  padding: 1rem;
+  display: flex;
+  border: solid 1px black;
 `;
-
 const CityContainer = styled.div`
-display : flex;
-flex-direction : column;
-padding : 1rem;
-margin-right:0.5rem;
-border : solid 1px black;
-width : 30%;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  margin-right: 0.5rem;
+  border: solid 1px black;
+  width: 30%;
 `;
 const Button = styled.button`
-margin : 0.1rem 0;
-padding : 0.1rem 0.5rem;
-border : solid 1px grey;
-background : transparent;
-width : auto;
+  margin: 0.1rem 0;
+  padding: 0.1rem 0.5rem;
+  border: solid 1px grey;
+  background: transparent;
+  width: auto;
 `;
 
 const App = () => {
-  
-  const [position,setPosition] = useState([49.195184999999995, 2.043914]);
+  const [position, setPosition] = useState([49.195184999999995, 2.043914]);
   const [map, setMap] = useState(null);
-  const [popupMessage, setPopupMessage]=useState("Bienvenue à Henonville !")
-  const [city, setCity] = useState(data.default.slice(0,100));
- 
-  
+  const [popupMessage, setPopupMessage] = useState("Bienvenue à Henonville !");
+  const [city, setCity] = useState(data.default.slice(0, 100));
+
   const changeCity = (e) => {
     const LatLng = CityPosition(e.currentTarget.name);
-      setPopupMessage("Bienvenue à "+e.currentTarget.name+ ' !')
-      setPosition(LatLng);
-      if (map) map.flyTo(LatLng,8);
-  }
-  return(
-      <GeneralContainer> 
-          <CityContainer>
-            {city.map((newdata) => 
-                <Button key={newdata.rank} onClick ={changeCity} name={newdata.city}>{newdata.city}</Button>                       
-            )}
-          <Pagination datasCity={city} setDatasCity={setCity} /> 
-          </CityContainer>  
-          <MapContainer id="mapid" center={position} zoom={8} scrollWheelZoom={false} whenCreated={map => setMap(map)}>
-              <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={position}>
-                <Popup>
-                  {popupMessage}
-                </Popup>
-              </Marker>
-          </MapContainer>
-      </GeneralContainer>
-  ) 
-}
+    setPopupMessage("Bienvenue à " + e.currentTarget.name + " !");
+    setPosition(LatLng);
+    if (map) map.flyTo(LatLng, 8);
+  };
+  return (
+    <GeneralContainer>
+      <CityContainer>
+        {city.map((newdata) => (
+          <Button key={newdata.rank} onClick={changeCity} name={newdata.city}>
+            {newdata.city}
+          </Button>
+        ))}
+        <Pagination datasCity={city} setDatasCity={setCity} />
+      </CityContainer>
+      <MapContainer
+        id="mapid"
+        center={position}
+        zoom={8}
+        scrollWheelZoom={false}
+        whenCreated={(map) => setMap(map)}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>{popupMessage}</Popup>
+        </Marker>
+      </MapContainer>
+    </GeneralContainer>
+  );
+};
 
 export default App;
--- src/component/CityPosition.jsx
+++ src/component/CityPosition.jsx
@@ -1,11 +1,12 @@
-import * as data from '../cities.json';
+import * as data from "../cities.json";
+
 const CityPosition = (name) => {
-        const DatasJson = data.default;
-        const cityName = (element) => element.city === name;
-        const cityIndex = DatasJson.findIndex(cityName);
-        const datasCity = data.default[cityIndex];
-        const cityPosition = [datasCity.latitude,datasCity.longitude]
-    return cityPosition;
-}
- 
+  const datasJson = data.default;
+  const cityName = (element) => element.city === name;
+  const cityIndex = datasJson.findIndex(cityName);
+  const datasCity = data.default[cityIndex];
+  const cityPosition = [datasCity.latitude, datasCity.longitude];
+  return cityPosition;
+};
+
 export default CityPosition;
